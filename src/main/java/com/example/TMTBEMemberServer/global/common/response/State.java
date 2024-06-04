870bc3d433e76f2c9c9e722b35b36ad26a1cbb56
[hoontaepark] <pthomj13@gmail.com>
2024-06-04 20:58:09 +0900

    Fix:yml수정

--- src/main/java/com/example/TMTBEMemberServer/global/common/response/State.java
+++ src/main/java/com/example/TMTBEMemberServer/global/common/response/State.java
@@ -5,14 +5,14 @@ import lombok.Getter;
 @Getter
 public enum State {
 
-    SIGNUP(1),//회원가입한 회원
-    SIGNIN(2), //로그인한 회원
-    SIGNOUT(3),//로그아웃한 회원
-    OUT(4);//탈퇴한 회원
+    SIGNUP("1"),//회원가입한 회원
+    SIGNIN("2"), //로그인한 회원
+    SIGNOUT("3"),//로그아웃한 회원
+    OUT("4");//탈퇴한 회원
 
-    private final int code;
+    private final String code;
 
-    State(int code) {
+    State(String code) {
         this.code = code;
     }
     

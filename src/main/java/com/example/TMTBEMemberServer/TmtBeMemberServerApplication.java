fe6fdca3007b826f419a6b9cc4e929f2a72c160a
[hoontaepark] <pthomj13@gmail.com>
2024-06-04 21:12:55 +0900

    Fix:배포수정

--- src/main/java/com/example/TMTBEMemberServer/TmtBeMemberServerApplication.java
+++ src/main/java/com/example/TMTBEMemberServer/TmtBeMemberServerApplication.java
@@ -1,10 +1,12 @@
 package com.example.TMTBEMemberServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class TmtBeMemberServerApplication {
 
 	public static void main(String[] args) {

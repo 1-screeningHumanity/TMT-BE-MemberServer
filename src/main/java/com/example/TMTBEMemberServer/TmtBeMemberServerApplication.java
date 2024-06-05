89fd8217d04effbc6f036721f5f8b552f294e630
[hoontaepark] <pthomj13@gmail.com>
2024-06-05 17:52:21 +0900

    Fix:yml파일ë 배포용으로 변경

--- src/main/java/com/example/TMTBEMemberServer/TmtBeMemberServerApplication.java
+++ src/main/java/com/example/TMTBEMemberServer/TmtBeMemberServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class TmtBeMemberServerApplication {
 
 	public static void main(String[] args) {

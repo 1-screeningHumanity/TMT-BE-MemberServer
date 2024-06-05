4c8ee520e5b33dbf05139a33e6db66960e7729c7
[hoontaepark] <pthomj13@gmail.com>
2024-06-05 17:51:13 +0900

    Feat:AccessToken 재발급 구현

--- src/main/java/com/example/TMTBEMemberServer/application/service/TokenService.java
+++ src/main/java/com/example/TMTBEMemberServer/application/service/TokenService.java
@@ -8,11 +8,13 @@ import com.example.TMTBEMemberServer.global.common.response.BaseResponseCode;
 import com.example.TMTBEMemberServer.global.common.token.DecodingToken;
 import com.example.TMTBEMemberServer.global.config.JWTUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TokenService implements TokenUsecase {
 
 
@@ -24,14 +26,19 @@ public class TokenService implements TokenUsecase {
     public ReAccessTokenDto reissueToken(String jwt) {
 
         String uuid = decodingToken.getUuid(jwt);
+        String withoutBearer = jwt.substring(7);
 
         if (jwt == null) {
             throw new CustomException(BaseResponseCode.EMPTY_TOKEN);
         }
-        String ReAccessToken = checkAccessToken(jwt,uuid);
+        log.info("2222222222222222222");
 
+        String reAccessToken = checkAccessToken(withoutBearer,uuid);
+
+
+        log.info("666666666666666666666");
         return ReAccessTokenDto.builder()
-                .AccessToken(ReAccessToken)
+                .AccessToken(reAccessToken)
                 .build();
     }
 
@@ -41,15 +48,21 @@ public class TokenService implements TokenUsecase {
 
         if (Boolean.TRUE.equals(check)) {
 
+
+            log.info("333333333333");
+            log.info("accesstoken{}",RefreshToken);
             //uuid를 key값으로 저장된 RefreshToken을 불러옴
             String storedRefreshToken = redisTemplate.opsForValue().get(uuid);
+            log.info("redisRefresh{}",storedRefreshToken);
 
             if(!RefreshToken.equals(storedRefreshToken)) {
+
+                log.info("444444444444444");
                 throw new CustomException(BaseResponseCode.WRONG_TOKEN);
             }
             return jwtUtil.remakeAccessToken(uuid);
         }
-
+        log.info("555555555555555");
         return new CustomException(BaseResponseCode.WRONG_TOKEN).getMessage();
     }
 }

a8d94a38fed430eef737131a6866ecf81389dbbd
[hoontaepark] <pthomj13@gmail.com>
2024-06-12 14:25:46 +0900

    Fix:yml수정

--- src/main/java/com/example/TMTBEMemberServer/TmtBeMemberServerApplication.java
+++ src/main/java/com/example/TMTBEMemberServer/TmtBeMemberServerApplication.java
@@ -1,7 +1,7 @@
 package com.example.TMTBEMemberServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
